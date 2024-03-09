eecfe268bfc3edc17902eff6d0422a88dd3689c9
aniket0727 <aniketkangude9060@gmail.com>
2024-03-09 17:26:53 +0530

    new update 1

--- class6.py
+++ class6.py
@@ -4,12 +4,14 @@ class first:
     def __init__(self,a1,name1 ):
         self.a=a1
         self.name=name1
+        print(name1)
         
     def info(self):
         print(self.a)
         print(self.name)
         print(self.s)
         print(first.s)
+        
         
         
 f1=first(10,"Java")
